60180a3f33dc21f019368a5f52a062734622e854
vysavely <s01g@ya.ru>
2023-10-23 23:30:13 +0300

    Oleg truten

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,10 @@
 package org.example;
 
+import javax.sound.midi.Soundbank;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello GSOM 444!");
+        System.out.println("Hello GSOM 555!");
+        System.out.println("11111");
     }
 }
